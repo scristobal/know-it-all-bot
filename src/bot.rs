4356e241fa099a23452a518865ce0aed3bfa5f76
Samuel Cristobal <el.tobal@gmail.com>
2023-03-09 13:25:52 +0100

    updated error handling

--- src/bot.rs
+++ src/bot.rs
@@ -1,7 +1,7 @@
 use crate::openai_client::reply;
 use log;
 
-use teloxide::{prelude::*, types::InputFile, utils::command::BotCommands, RequestError};
+use teloxide::{prelude::*, utils::command::BotCommands, RequestError};
 
 #[derive(BotCommands, Clone)]
 #[command(
@@ -18,7 +18,7 @@ pub async fn answer_cmd_repl(
     msg: Message,
     cmd: Command,
 ) -> Result<(), RequestError> {
-    log::info!("new job from {}", msg.chat.username().unwrap_or("unknown"));
+    log::info!("new cmd from {}", msg.chat.username().unwrap_or("unknown"));
 
     let results = match cmd {
         Command::Ask(prompt) => reply(prompt).await,
@@ -26,7 +26,13 @@ pub async fn answer_cmd_repl(
 
     match results {
         Err(e) => match e {
-            OpenAIError => log::error!(""),
+            async_openai::error::OpenAIError::Reqwest(e) => log::error!("{}", e),
+            async_openai::error::OpenAIError::ApiError(e) => log::error!("{}", e.message),
+            async_openai::error::OpenAIError::JSONDeserialize(e) => log::error!("{}", e),
+            async_openai::error::OpenAIError::FileSaveError(e) => log::error!("{}", e),
+            async_openai::error::OpenAIError::FileReadError(e) => log::error!("{}", e),
+            async_openai::error::OpenAIError::StreamError(e) => log::error!("{}", e),
+            async_openai::error::OpenAIError::InvalidArgument(e) => log::error!("{}", e),
         },
         Ok(results) => {
             for result in results {
